3e2265713c8ac106420e061a91bb582a1b761d04
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2022-01-20 22:02:20 +0100

    feat: add cli parser to support overriding default variables

--- lib/app/AppRunner.ts
+++ lib/app/AppRunner.ts
@@ -1,14 +1,30 @@
 import { ComponentsManager } from 'componentsjs';
 import * as Path from 'path';
+import yargs, { Arguments } from 'yargs';
 import { IApp } from './IApp';
 
 export default class AppRunner {
   async start() {
+    const argv = await yargs(process.argv.slice(2))
+      .usage('node ./dist/start.js [args]')
+      .options({
+        config: { type: 'string', alias: 'c', default: 'config/config.jsonld', requiresArg: true },
+        entrypoint: { type: 'string', alias: 'e', default: 'urn:@iotakingdoms/auth:app', requiresArg: true },
+        logLevel: { type: 'string', alias: 'l', default: 'INFO', requiresArg: true },
+        port: { type: 'number', alias: 'p', default: 8080, requiresArg: true },
+      })
+      .parse();
+
+    const variables = {
+      'urn:@iotakingdoms/auth:variable:port': argv.port,
+      'urn:@iotakingdoms/auth:variable:logLevel': argv.logLevel,
+    };
+    
     const manager = await ComponentsManager.build({
       mainModulePath: Path.join(__dirname, '/../..'),
     });
-    await manager.configRegistry.register('config/config.jsonld');
-    const app = await manager.instantiate('urn:@iotakingdoms/auth:app') as IApp;
+    await manager.configRegistry.register(argv.config);
+    const app = await manager.instantiate(argv.entrypoint, { variables }) as IApp;
     await app.start();
   }
 }
