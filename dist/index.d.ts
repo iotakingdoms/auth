cf786ed95f50020a93ca2914022957a284a46deb
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2022-02-02 17:47:34 +0100

    stuff

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './lib/Test1App';
-export * from './lib/Test1Component';
-//# sourceMappingURL=index.d.ts.map
--- dist/lib/Test1Component.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Test1Component {
-    constructor();
-    sayHi(): void;
-}
-//# sourceMappingURL=Test1Component.d.ts.map
